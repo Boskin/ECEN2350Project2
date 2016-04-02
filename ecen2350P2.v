6245d367ab3e6eceba03954a74f57927ae6aeb93
unknown <boskin.erkocevic99@yahoo.com>
2016-04-02 11:02:44 -0600

    Made the intermediate signal opSelect an output

--- ecen2350P2.v
+++ ecen2350P2.v
@@ -10,6 +10,7 @@ module ecen2350P2(
     output cOut,
     output overflow,
     output sumsEqual,
+    output opSelect,
     input [3:0] a,
     input [3:0] b,
     input aSign,
@@ -42,14 +43,13 @@ module ecen2350P2(
     
     // These connect to registers that store the current options
     wire adderSelect;
-    wire opSelect;
     
     // Will be used to complete 3's compliments for opperand b if necessary
     wire carryIn;
     
     // Use the three's compliment of a if a is indicated to be negative
-    ternaryThreesCompliment#(3)(aNeg, {2'b00, a});
-    mux2to1#(6)(aActual, {2'b00, a}, aNeg, aSign);
+    ternaryThreesCompliment#(3) comp0(aNeg, {2'b00, a});
+    mux2to1#(6) mux0(aActual, {2'b00, a}, aNeg, aSign);
     
     /* Compute the carryIn based on what the operation is and the sign of b,
        if b is negative and subtraction is selected, just perform a + b,
@@ -57,9 +57,9 @@ module ecen2350P2(
        if neither is selected, again, perform a + b */
     xor(carryIn, opSelect, bSign);
     // Compute the 2's compliment
-    ternaryTwosCompliment#(3)(bComp, {2'b00, b});
+    ternaryTwosCompliment#(3) comp1(bComp, {2'b00, b});
     // Select the 2's compliment if necessary
-    mux2to1#(6)(bActual, {2'b00, b}, bComp, carryIn);
+    mux2to1#(6) mux1(bActual, {2'b00, b}, bComp, carryIn);
     
     // Set up the toggle registers for the user to configure what is outputted
     buttonToggleRegister tog0(adderSelect, adderButton, clk);
@@ -78,9 +78,9 @@ module ecen2350P2(
         rippleCarryOverflow == carryLookAheadOverflow;
     
     // Choose the result to be displayed from the selected adder
-    mux2to1#(6) mux1(sum, rippleCarrySum, carryLookAheadSum, adderSelect);
-    mux2to1#(1) mux2(cOut, rippleCarryCout, carryLookAheadCout, adderSelect);
-    mux2to1#(1) mux3(overflow, rippleCarryOverflow, carryLookAheadOverflow, adderSelect);
+    mux2to1#(6) mux2(sum, rippleCarrySum, carryLookAheadSum, adderSelect);
+    mux2to1#(1) mux3(cOut, rippleCarryCout, carryLookAheadCout, adderSelect);
+    mux2to1#(1) mux4(overflow, rippleCarryOverflow, carryLookAheadOverflow, adderSelect);
     
     // Display the three-digit result
     hexDisplayDecoder disp0(sumDisp[6:0], sum[1:0]); // Least significant
